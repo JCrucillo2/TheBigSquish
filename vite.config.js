5c3e4c9410ee12bd435454119441507a8d9d9f53
Junnel Crucillo <junnelcrucillo@Junnels-MacBook-Pro.local>
2023-11-07 15:25:21 -0700

    Updated paths

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,10 @@ export default defineConfig({
 		// alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
 		alias: {
 			"@": path.resolve(__dirname, "./src/"),
+			"@/sassStyles": path.resolve(__dirname, "./src/sassStyles"),
+			"@/assets": path.resolve(__dirname, "./src/assets"),
+			"@/components": path.resolve(__dirname, "./src/components"),
+			"@/pages": path.resolve(__dirname, "./src/pages"),
 		},
 	},
 });
